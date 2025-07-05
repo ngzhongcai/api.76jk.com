d2cdc12f714b5987cf6c3c31167b65f50ad06957
ngzhongcai <74141331+ngzhongcai@users.noreply.github.com>
2025-07-05 19:40:35 +0800

    message

--- 76JK_GenerateStatic.js
+++ 76JK_GenerateStatic.js
@@ -8,26 +8,26 @@ const sharp= require("sharp");
 exports.handler= function(event, context, callback) {
   event.body= JSON.parse(event.Records[0].Sns.Message); console.log(event.body);
   const timings= []; var timetaken= 0; var now= Math.round(new Date().getTime()); var last= Math.round(new Date().getTime()); event.body.now= now;
-  generateStatic(event, function(err, res) {
-    now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("GENERATE_STATIC::" + (now- last)); last= now;
-    if(err) { context.fail("501::76JK_GENERATE_STATIC::GENERATE_STATIC::" + err.toString()); return; }
-    uploadStatic(event, function(err, res) {
-      now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("UPLOAD_STATIC::" + (now- last)); last= now;
-      if(err) { context.fail("502::76JK_GENERATE_STATIC::UPLOAD_STATIC::" + err.toString()); return; }
-      createInvalidation(event, function(err, res) {
-        now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("CREATE_INVALIDATION::" + (now- last)); last= now;
-        if(err) { context.fail("503::76JK_GENERATE_STATIC::CREATE_INVALIDATION::" + err.toString()); return; }
-        downloadPhoto(event, function(err, res) {
-          now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("DOWNLOAD_PHOTO::" + (now- last)); last= now;
-          if(err) { context.fail("504::76JK_GENERATE_STATIC::DOWNLOAD_PHOTO::" + err.toString()); return; }
-          event.body.photo= res;
-          compressPhoto(event, function(err, res) {
-            now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("COMPRESS_PHOTO::" + (now- last)); last= now;
-            if(err) { context.fail("505::76JK_GENERATE_STATIC::COMPRESS_PHOTO::" + err.toString()); return; }
-            event.body.compressed= res;
-            uploadPhoto(event, function(err, res) {
-              now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("UPLOAD_PHOTO::" + (now- last)); last= now;
-              if(err) { context.fail("506::76JK_GENERATE_STATIC::UPLOAD_PHOTO::" + err.toString()); return; }
+  downloadPhoto(event, function(err, res) {
+    now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("DOWNLOAD_PHOTO::" + (now- last)); last= now;
+    if(err) { context.fail("501::76JK_GENERATE_STATIC::DOWNLOAD_PHOTO::" + err.toString()); return; }
+    event.body.photo= res;
+    compressPhoto(event, function(err, res) {
+      now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("COMPRESS_PHOTO::" + (now- last)); last= now;
+      if(err) { context.fail("502::76JK_GENERATE_STATIC::COMPRESS_PHOTO::" + err.toString()); return; }
+      event.body.compressed= res;
+      uploadPhoto(event, function(err, res) {
+        now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("UPLOAD_PHOTO::" + (now- last)); last= now;
+        if(err) { context.fail("503::76JK_GENERATE_STATIC::UPLOAD_PHOTO::" + err.toString()); return; }
+        generateStatic(event, function(err, res) {
+          now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("GENERATE_STATIC::" + (now- last)); last= now;
+          if(err) { context.fail("504::76JK_GENERATE_STATIC::GENERATE_STATIC::" + err.toString()); return; }
+          uploadStatic(event, function(err, res) {
+            now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("UPLOAD_STATIC::" + (now- last)); last= now;
+            if(err) { context.fail("505::76JK_GENERATE_STATIC::UPLOAD_STATIC::" + err.toString()); return; }
+            createInvalidation(event, function(err, res) {
+              now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("CREATE_INVALIDATION::" + (now- last)); last= now;
+              if(err) { context.fail("506::76JK_GENERATE_STATIC::CREATE_INVALIDATION::" + err.toString()); return; }
               const obj= {}; obj.timings= timings; obj.timetaken= timetaken; console.log(obj);
               context.succeed({ "response": res });
             });
@@ -37,6 +37,37 @@ exports.handler= function(event, context, callback) {
     });
   });
 } 
+
+const downloadPhoto= function(event, callback) {
+  if(!event.body.entryId) { callback(); return; }
+  const params= {
+    Bucket: "console.76jk.com",
+    Key: "statics/images/" + event.body.tagId + "/ORIGINAL_" + event.body.entryId + ".jpeg",
+  } 
+  s3.getObject(params, function(err, res) { 
+    err ? callback(err) : callback(null, res.Body);
+  });
+};
+
+const compressPhoto= function(event, callback) { 
+  if(!event.body.entryId) { callback(); return; }
+  sharp(event.body.photo).rotate().resize(1080).jpeg({ quality: 80 }).toBuffer(function(err, res) {
+    err ? callback(err) : callback(null, res);
+  });
+}
+
+const uploadPhoto= function(event, callback) { 
+  if(!event.body.entryId) { callback(); return; }
+  const params= {
+    Bucket: "console.76jk.com",
+    Key: "statics/images/" + event.body.tagId + "/" + event.body.entryId + ".jpeg",
+    Body: event.body.compressed,
+    ContentType: "image/jpeg"
+  };
+  s3.putObject(params, function(err, data) {
+    err ? callback(err) : callback(null, data);
+  });
+}
 
 const generateStatic= async function(event, callback) {
   const chromiumModule= await import("@sparticuz/chromium");
@@ -86,36 +117,5 @@ const createInvalidation= function(event, callback) {
   }
   cloudfront.createInvalidation(params, function(err, res) {
     err ? callback(err) : callback(null, res);
-  });
-}
-
-const downloadPhoto= function(event, callback) {
-  if(!event.body.entryId) { callback(); return; }
-  const params= {
-    Bucket: "console.76jk.com",
-    Key: "statics/images/" + event.body.tagId + "/ORIGINAL_" + event.body.entryId + ".jpeg",
-  } 
-  s3.getObject(params, function(err, res) { 
-    err ? callback(err) : callback(null, res.Body);
-  });
-};
-
-const compressPhoto= function(event, callback) { 
-  if(!event.body.entryId) { callback(); return; }
-  sharp(event.body.photo).rotate().resize(1080).jpeg({ quality: 80 }).toBuffer(function(err, res) {
-    err ? callback(err) : callback(null, res);
-  });
-}
-
-const uploadPhoto= function(event, callback) { 
-  if(!event.body.entryId) { callback(); return; }
-  const params= {
-    Bucket: "console.76jk.com",
-    Key: "statics/images/" + event.body.tagId + "/" + event.body.entryId + ".jpeg",
-    Body: event.body.compressed,
-    ContentType: "image/jpeg"
-  };
-  s3.putObject(params, function(err, data) {
-    err ? callback(err) : callback(null, data);
   });
 }
