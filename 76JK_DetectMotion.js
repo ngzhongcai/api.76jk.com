446f140058b815b9d3605341e98025f14cd5d7f1
M4 <cai@Mac.localdomain>
2025-09-29 21:15:27 +0800

    message

--- 76JK_DetectMotion.js
+++ 76JK_DetectMotion.js
@@ -8,10 +8,10 @@ exports.handler= function(event, context, callback) {
     const timings= []; var timetaken= 0; var now= Math.round(new Date().getTime()); var last= Math.round(new Date().getTime()); event.body.now= now;
     verifyToken(event, function(err, res) {
         now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("VERIFY_TOKEN::" + (now- last)); last= now;
-        if(err) { context.fail("501::76JK_DETECT_PERSON::VERIFY_TOKEN::" + err.toString()); return; }
+        if(err) { context.fail("501::76JK_DETECT_MOTION::VERIFY_TOKEN::" + err.toString()); return; }
         publishToDisplay(event, function(err, res) {
             now= Math.round(new Date().getTime()); timetaken= timetaken+ now- last; timings.push("PUBLISH_TO_DISPLAY::" + (now- last)); last= now;
-            if(err) { context.fail("501::76JK_DETECT_PERSON::PUBLISH_TO_DISPLAY::" + err.toString()); return; }
+            if(err) { context.fail("501::76JK_DETECT_MOTION::PUBLISH_TO_DISPLAY::" + err.toString()); return; }
             const obj= {}; obj.timings= timings; obj.timetaken= timetaken; console.log(obj);
             const response= { tagId: event.body.tagId };
             context.succeed({ "response": response });
@@ -29,7 +29,7 @@ const verifyToken= function(event, callback) {
 }
 
 var publishToDisplay= function(event, callback) {
-    var obj= {}; obj.action= "human"; obj.view= "gate"; var payload= JSON.stringify(obj);
+    var obj= {}; obj.action= "motion"; obj.view= "bonsai"; var payload= JSON.stringify(obj);
     var params= { topic: "NGFAMILY/DISPLAY@NGFAMILY.COM", payload: payload, qos: 0 }
     iotdata.publish(params, function(err) {
         err ? callback(err) : callback();
